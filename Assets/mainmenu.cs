aeecf4aec9b29e5ea28babc4ce078cba6143b09a
moiasdkj <officialdeluxo@gmail.com>
2025-03-18 09:29:36 +0200

    main menu doing välitallenus

---
+++ Assets/mainmenu.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class mainmenu : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        Debug.Log("Alojtetaan peli");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("suljetaan pelji");
+    }
+}
